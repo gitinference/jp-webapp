59d3a56547237221efbe7ea4c95af27b03e3c65f
LUIS J SERRATO DIAZ <luisjomyr02@gmail.com>
2025-05-15 10:30:40 -0400

    Revert "creación del template base y modificación del formulario JP-304, cambio en la version del proyecto"
    This reverts commit 7b4b1bec25955347bf6122eba86170db76ed7762.

--- jp_webapp/settings.py
+++ jp_webapp/settings.py
@@ -77,7 +77,6 @@ TEMPLATES = [
                 "django.contrib.auth.context_processors.auth",
                 "django.contrib.messages.context_processors.messages",
             ],
-            'debug': True,
         },
     },
 ]
