9269772f60291da322d685e76218c43c2b79012f
LUIS J SERRATO DIAZ <luisjomyr02@gmail.com>
2025-05-19 14:43:45 -0400

    botones para exportar csv y cambios en js css

--- staticfiles/js/exportcsv.js
+++ staticfiles/js/exportcsv.js
@@ -1,21 +1,11 @@
 function downloadCSV(frec, lev) {
-    const frequency = document.getElementById(frec).value.trim();
-    const allowedFrequencies = ['quarterly', 'monthly'];
-    let frequencyLower = frequency.toLowerCase();
-    if (!allowedFrequencies.includes(frequencyLower)) {
-        console.error('Invalid frequency value');
-        return;
-    }
+    const frequency = document.getElementById(frec).value;
+    frequencyLower = frequency.toLowerCase();
     if (frequencyLower === 'quarterly') {
         frequencyLower = 'qrt';
     }
-    const levelLower = lev.toLowerCase().trim();
-    const allowedLevels = ['low', 'medium', 'high']; // Example allowed values
-    if (!allowedLevels.includes(levelLower)) {
-        console.error('Invalid level value');
-        return;
-    }
-    const url = `http://192.168.50.24:7001/files/trade/jp/?level=${encodeURIComponent(levelLower)}&time_frame=${encodeURIComponent(frequencyLower)}&agr=false&group=false`;
+    levelLower = lev.toLowerCase();
+    let url = `http://192.168.50.24:7001/files/trade/jp/?level=${levelLower}&time_frame=${frequencyLower}&agr=false&group=false`;
     //alert(url)
 
     const a = document.createElement('a');
