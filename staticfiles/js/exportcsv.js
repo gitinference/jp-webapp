c454999170f416f60791dfdab7efb8f075036d19
LUIS J SERRATO DIAZ <luisjomyr02@gmail.com>
2025-05-20 09:04:50 -0400

    ajuste de product ranking en el css

--- staticfiles/js/exportcsv.js
+++ staticfiles/js/exportcsv.js
@@ -1,13 +1,27 @@
 function downloadCSV(frec, lev, api) {
-    const frequency = document.getElementById(frec).value;
-    frequencyLower = frequency.toLowerCase();
-    if (frequencyLower === 'quarterly') {
-        frequencyLower = 'qrt';
+
+    
+    const frequency = "";
+    let frequencyLower = "";
+    let levelLower = "";
+    let apiurl = "";
+    if (frec != "") {
+
+        frequency = document.getElementById(frec).value;
+        frequencyLower = frequency.toLowerCase();
+        if (frequencyLower === 'quarterly') {
+            frequencyLower = 'qrt';
+        }
     }
-    levelLower = lev.toLowerCase();
+    if (lev != "") {
+
+        levelLower = lev.toLowerCase();
+    }
     apiurl = api;
     let url = `http://192.168.50.24:7001/${apiurl}/?level=${levelLower}&time_frame=${frequencyLower}&agr=false&group=false`;
-    //alert(url)
+
+/*     alert(apiurl)
+    return false */
 
     const a = document.createElement('a');
     a.href = url;
