3cee37daa772c13aead53425346f9f01a156c265
LUIS J SERRATO DIAZ <luisjomyr02@gmail.com>
2025-05-20 09:40:11 -0400

    se realizan nuevamente los cambios en el html y js

--- staticfiles/js/exportcsv.js
+++ staticfiles/js/exportcsv.js
@@ -1,12 +1,27 @@
-function downloadCSV(frec, lev) {
-    const frequency = document.getElementById(frec).value;
-    frequencyLower = frequency.toLowerCase();
-    if (frequencyLower === 'quarterly') {
-        frequencyLower = 'qrt';
+function downloadCSV(frec, lev, api) {
+
+    
+    const frequency = "";
+    let frequencyLower = "";
+    let levelLower = "";
+    let apiurl = "";
+    if (frec != "") {
+
+        frequency = document.getElementById(frec).value;
+        frequencyLower = frequency.toLowerCase();
+        if (frequencyLower === 'quarterly') {
+            frequencyLower = 'qrt';
+        }
     }
-    levelLower = lev.toLowerCase();
-    let url = `http://192.168.50.24:7001/files/trade/jp/?level=${levelLower}&time_frame=${frequencyLower}&agr=false&group=false`;
-    //alert(url)
+    if (lev != "") {
+
+        levelLower = lev.toLowerCase();
+    }
+    apiurl = api;
+    let url = `http://192.168.50.24:7001/${apiurl}/?level=${levelLower}&time_frame=${frequencyLower}&agr=false&group=false`;
+
+/*     alert(apiurl)
+    return false */ 
 
     const a = document.createElement('a');
     a.href = url;
