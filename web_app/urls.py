bf65c467c03f32cc8c98af8c580f7fe72823c4ed
Diego F. Vasquez P. <85289576+vapdif@users.noreply.github.com>
2025-06-12 10:37:22 -0400

    enrutamiento cuestionarios Gastos, Software, entretenimiento (#221)
    * enrutamiento cuestionarios Gastos, Software, entretenimiento
    
    * 212 - update rutas cuestionarios -gastos intangibles
    
    * 212 - update ruta cuestionario gastos comerciales -gastos intangibles

--- web_app/urls.py
+++ web_app/urls.py
@@ -100,15 +100,17 @@ urlpatterns = [
     re_path(r"^idh-index/csv/$", views.idh_index_csv, name="idh_index_csv"),    # for IDH index CSV download
     
     # Gastos intangibles
-    
+   
     re_path(r"^medicion-activos-ID-en-activos-intangibles/$", views.Medicion_ID_archivos_intangibles, name="Medicion-ID-archivos-intangibles"),
     re_path(r"^concesiones-recursos-naturales-activos-intangibles/$", views.Medicion_concesiones_recursos_naturales, name="Medicion-concesiones-recursos-naturales"),
     re_path(r"^medicion-licencias-propiedad-intelectual-marcas-registradas/$", views.Medicion_licencias_uso_propiedad_intelectual_o_marcas_registradas, name="medicion-licencias-propiedad-intelectual-marcas-registradas"),
     re_path(r"^medicion-gastos-comercializacion-formacion-marca/$", views.Medicion_gastos_comercializacion_formacion_marca, name="medicion-gastos-comercializacion-formacion-marca"),
     re_path(r"^medicion-productos-entretenimiento-activos-intangibles/$", views.IP_230_qtr, name="IP-230-qtr"),
     re_path(r"^medicion-software-bases-de-datos-activos-intangibles/$", views.IP_230_qtr, name="IP-230-qtr"),
+    re_path(r"^cuestionario-gastos-de-comercialización-y-formación-de-marca/$", views.Cuestionario_gastos_de_comercialización_y_formación_de_marca, name="Cuestionario-gastos-de-comercialización-y-formación-de-marca"),   # cuestionario gastos comercialización y formación de marca
+    re_path(r"^cuestionario-productos-de-entretenimiento-como-activos-intangibles/$", views.Cuestionario_productos_de_entretenimiento_como_activos_intangibles, name="Cuestionario-productos-de-entretenimiento-como-activos-intangibles"),   # cuestionario medir software bases de datos como activos intangibles
+    re_path(r"^cuestionario-medir-software-bases-de-datos-como-activos-intangibles/$", views.Cuestionario_medir_software_bases_de_datos_como_activos_intangibles, name="Cuestionario-medir-software-bases-de-datos-como-activos-intangibles"),   # cuestionario medir software bases de datos como activos intangibles
     re_path(r"^definición-gastos-comercialización-y-formacion-marca/$", views.Definicion_gastos_comercialización_y_formación_de_marca, name="definición-gastos-comercialización-y-formacion-marca"),
-    
-   
+
 ]
 
