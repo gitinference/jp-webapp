167b76d37aadcf9814f7b40a9b3bea2f27ab4915
NatiPerez459 <natalia.roldan@upr.edu>
2025-06-13 15:35:30 -0400

    added function to download awards by category

--- staticfiles/js/exportcsv.js
+++ staticfiles/js/exportcsv.js
@@ -30,4 +30,50 @@ function downloadCSV(frec, lev, api) {
     a.click();
     document.body.removeChild(a);
 }
+
+function downloadAwardsCategoryCSV(frec, sec, thrd, cat, api) {
+    const apienv = window.API_URL;
+    
+    let frequency = "";
+    let second_dropdown = "";
+    let category = "";
+    let third_dropdown = "";
+
+    let apiurl = "";
+    let baseUrl = "";
+    if (frec != "" && sec != "" && cat != "") {
+        frequency = document.getElementById(frec).value;
+        second_dropdown = document.getElementById(sec).value;
+        category = document.getElementById(cat).value;
+    }
+
+    if (frec == 'yearly') {
+        if (thrd != "") {
+            third_dropdown = document.getElementById(thrd).value;
+        }
+    }
+
+    apiurl = api;
+    baseUrl = apienv+apiurl+"/?";
+    
+    let params = [];
+    if (frequency) params.push("time_frame=" + encodeURIComponent(frequency));
+    if (category) params.push("third_dropdown=" + encodeURIComponent(category));
+
+    if (frequency == "yearly") {
+        if (second_dropdown) params.push("dropdown=" + encodeURIComponent(second_dropdown));
+    } else {
+        if (third_dropdown) params.push("dropdown=" + encodeURIComponent(third_dropdown));
+        if (second_dropdown) params.push("second_dropdown=" + encodeURIComponent(second_dropdown));
+    }
+
+    url = baseUrl + params.join("&");
+    const a = document.createElement('a');
+    a.href = url;
+    document.body.appendChild(a);
+    a.click();
+    document.body.removeChild(a);
+}
+
+window.downloadAwardsCategoryCSV = downloadAwardsCategoryCSV;
 window.downloadCSV = downloadCSV;
