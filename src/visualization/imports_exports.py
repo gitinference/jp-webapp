d1cd0ae2263b31ebf3f4ca71f2154ca7101c31e2
NatiPerez459 <natalia.roldan@upr.edu>
2025-06-18 15:35:27 -0400

    set import export year start at 2025

--- src/visualization/imports_exports.py
+++ src/visualization/imports_exports.py
@@ -22,14 +22,14 @@ def web_app_imports_exports(request):
 
         if frequency is None and second_dropdown is None:
             frequency = "Yearly"
-            second_dropdown = 2009
+            second_dropdown = 2025
         
         imports_graph = requests.get(
             f"{api}graph/import-export/?level=country&time_frame=yearly&datetime=2009&group=false&frequency={frequency}&second_dropdown={second_dropdown}&third_dropdown={third_dropdown}&type=imports"
         )
     else:
         imports_graph = requests.get(
-            f"{api}graph/import-export/?level=country&time_frame=yearly&group=false&frequency=Yearly&second_dropdown=2009&datetime=2009&type=imports"
+            f"{api}graph/import-export/?level=country&time_frame=yearly&group=false&frequency=Yearly&second_dropdown=2025&datetime=2025&type=imports"
         )
 
     imports = imports_graph.json()
@@ -45,14 +45,14 @@ def web_app_imports_exports(request):
 
         if frequency_2 is None and second_dropdown_2 is None:
             frequency_2 = "Yearly"
-            second_dropdown_2 = 2009
+            second_dropdown_2 = 2025
 
         exports_graph = requests.get(
             f"{api}graph/import-export/?level=country&time_frame=yearly&datetime=2009&group=false&frequency={frequency_2}&second_dropdown={second_dropdown_2}&third_dropdown={third_dropdown_2}&type=exports"
         )
     else:
         exports_graph = requests.get(
-            f"{api}graph/import-export/?level=country&time_frame=yearly&group=false&frequency=Yearly&second_dropdown=2009&datetime=2009&type=exports"
+            f"{api}graph/import-export/?level=country&time_frame=yearly&group=false&frequency=Yearly&second_dropdown=2025&datetime=2025&type=exports"
         )
 
     exports = exports_graph.json()
