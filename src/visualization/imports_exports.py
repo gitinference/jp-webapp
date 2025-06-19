1fd2ce0396e10b8bf32a835ed763fa6a7501b2b6
NatiPerez459 <natalia.roldan@upr.edu>
2025-06-19 15:23:57 -0400

    added hts drop to import export

--- src/visualization/imports_exports.py
+++ src/visualization/imports_exports.py
@@ -19,20 +19,22 @@ def web_app_imports_exports(request):
         frequency = request.POST.get("frequency")
         second_dropdown = request.POST.get("second_dropdown")
         third_dropdown = request.POST.get("third_dropdown")
+        level_filter = request.POST.get("hts_codes")
 
         if frequency is None and second_dropdown is None:
             frequency = "Yearly"
             second_dropdown = 2024
+            level_filter = '0000'
         
         imports_graph = requests.get(
-            f"{api}graph/import-export/?level=country&time_frame=yearly&datetime=2009&group=false&frequency={frequency}&second_dropdown={second_dropdown}&third_dropdown={third_dropdown}&type=imports"
+            f"{api}graph/import-export/?level=country&time_frame=yearly&datetime=2009&group=false&frequency={frequency}&second_dropdown={second_dropdown}&third_dropdown={third_dropdown}&type=imports&level_filter={level_filter}"
         )
     else:
         imports_graph = requests.get(
             f"{api}graph/import-export/?level=country&time_frame=yearly&group=false&frequency=Yearly&second_dropdown=2024&datetime=2024&type=imports"
         )
 
-    imports = imports_graph.json()
+    imports, hts_codes = imports_graph.json()
 
     # ------------------------------------------------------------------------------------------------------------------------------------------------------------
 
@@ -42,27 +44,30 @@ def web_app_imports_exports(request):
         frequency_2 = request.POST.get("frequency_2")
         second_dropdown_2 = request.POST.get("second_dropdown_2")
         third_dropdown_2 = request.POST.get("third_dropdown_2")
+        level_filter_2 = request.POST.get("hts_codes_2")
 
         if frequency_2 is None and second_dropdown_2 is None:
             frequency_2 = "Yearly"
             second_dropdown_2 = 2024
+            level_filter_2 = '0000'
 
         exports_graph = requests.get(
-            f"{api}graph/import-export/?level=country&time_frame=yearly&datetime=2009&group=false&frequency={frequency_2}&second_dropdown={second_dropdown_2}&third_dropdown={third_dropdown_2}&type=exports"
+            f"{api}graph/import-export/?level=country&time_frame=yearly&datetime=2009&group=false&frequency={frequency_2}&second_dropdown={second_dropdown_2}&third_dropdown={third_dropdown_2}&type=exports&level_filter={level_filter_2}"
         )
     else:
         exports_graph = requests.get(
             f"{api}graph/import-export/?level=country&time_frame=yearly&group=false&frequency=Yearly&second_dropdown=2024&datetime=2024&type=exports"
         )
 
-    exports = exports_graph.json()
+    exports, hts_codes_2 = exports_graph.json()
 
     # ------------------------------------------------------------------------------------------------------------------------------------------------------------
 
     context = {
         "imports": imports,
         "exports": exports,
-        "api": api
+        "api": api,
+        **hts_codes
     }
 
     return render(request, "imports_exports.html", context)
