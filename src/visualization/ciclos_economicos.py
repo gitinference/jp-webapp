9a47c628bdfc1483a78990e6c4486daab677ff45
NatiPerez459 <natalia.roldan@upr.edu>
2025-06-20 10:31:36 -0400

    added new cycles graph

---
+++ src/visualization/ciclos_economicos.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render
+import requests
+from env import get_db_credentials
+
+creds = get_db_credentials()
+
+api = creds[6]
+
+def web_app_ciclos_economicos(request):
+
+    if request.method == "POST":
+        column = request.POST.get("columns")
+    else:
+        column = request.GET.get("columns", 'indice_de_actividad_economica')
+
+    # Fetch graph from the API
+    response = requests.get(f"{api}graph/jp/cycles/?column={column}")
+    ciclos_economicos_html, columns = response.json()
+
+    return render(request, "ciclos_economicos.html", {"ciclos_economicos": ciclos_economicos_html, "api": api, "selected_column": column, **columns})
