2fbd3cfea2866728ebe478cd946aa4f61513cbe5
LUIS J SERRATO DIAZ <luisjomyr02@gmail.com>
2025-06-23 13:13:24 -0400

    fix empleo por industria

--- src/visualization/empleo_industria.py
+++ src/visualization/empleo_industria.py
@@ -8,16 +8,5 @@ api = creds[6]
 
 def empleo_industria(request):
   
-  naics = None
-  
-  if request.method == "POST":
-    naics_time = request.POST.get("naics_time")
-  else:
-    naics_time = "1111"
-    
-  response = requests.get(f"{api}graph/naics/?naics_code={naics_time}")
-  graph, naics = response.json()
-    
-  html_graph = f'<div style="padding-left: 120px;">{graph}</div>'
 
-  return render(request, "empleo_industria.html", {'graph': html_graph, 'api': api, 'naics_time_post': naics_time, **naics})
+  return render(request, "empleo_industria.html")
