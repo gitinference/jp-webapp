06de1d9249cde3b6d2db38e2a9559d050cbaf732
NatiPerez459 <natalia.roldan@upr.edu>
2025-06-24 12:13:24 -0400

    updated and united demographic graphs

---
+++ src/visualization/demographic.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render
+import requests
+from env import get_db_credentials
+
+creds = get_db_credentials()
+
+api = creds[6]
+
+def web_app_demographic(request):
+    if request.method == "POST":
+        frequency = request.POST.get("frequency").lower()
+        column = request.POST.get("columns")
+    else:
+        frequency = request.GET.get("frequency", "yearly").lower()
+        column = request.GET.get("columns", 'componentes')
+
+    response = requests.get(f"{api}graph/demographic/?time_frame={frequency}&column={column}")
+    demographic_html, columns = response.json()
+
+    return render(request, "demograficos.html", {"graph": demographic_html, **columns, 'selected_frequency': frequency, 'selected_column': column, "api" : api,})
