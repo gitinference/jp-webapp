c431907c60f169f90a3a4960bc4c65ec0497b486
NatiPerez459 <70607250+NatiPerez459@users.noreply.github.com>
2025-06-25 17:33:46 -0400

    337 nomina (#338)
    * added nomina graphs
    
    * fix page, columns, starting values
    
    * updated toml
    
    ---------
    
    Co-authored-by: Alejandro Ouslan <alejandro.ouslan@upr.edu>

--- src/visualization/empleo_industria.py
+++ src/visualization/empleo_industria.py
@@ -7,6 +7,42 @@ creds = get_db_credentials()
 api = creds[6]
 
 def empleo_industria(request):
-  
+  if request.method == "POST":
+    frequency = request.POST.get("frequency").lower()
+    frequency_2 = request.POST.get("frequency_2").lower()
+    frequency_3 = request.POST.get("frequency_3").lower()
+    column = request.POST.get("columns")
+    column_2 = request.POST.get("columns_2")
+    column_3 = request.POST.get("columns_3")
+  else:
+    frequency = "yearly"
+    frequency_2 = "yearly"
+    frequency_3 = "yearly"
+    column = 'Accounting, Tax Preparation, Bookkeeping, and Payroll Services'
+    column_2 = 'Accounting, Tax Preparation, Bookkeeping, and Payroll Services'
+    column_3 = 'Accounting, Tax Preparation, Bookkeeping, and Payroll Services'
 
-  return render(request, "empleo_industria.html")
+  # Fetch graph from the API
+  response = requests.get(f"{api}graph/nomina/?time_frame={frequency}&data_type=nivel&naics_desc={column}").json()
+  nomina_html, context = response
+
+  response = requests.get(f"{api}graph/nomina/?time_frame={frequency_2}&data_type=cambio_porcentual&naics_desc={column_2}").json()
+  cambio_porcentual_html, context = response
+
+  response = requests.get(f"{api}graph/nomina/?time_frame={frequency_3}&data_type=primera_diferencia&naics_desc={column_3}").json()
+  primera_diferencia_html, context = response
+
+  return render(request, "empleo_industria.html", 
+          {
+              "nomina": nomina_html, 
+              "api": api, 
+              "cambio_porcentual": cambio_porcentual_html, 
+              "primera_diferencia": primera_diferencia_html, 
+              **context, 
+              "selected_frequency": frequency, 
+              "selected_frequency_2": frequency_2,
+              "selected_frequency_3": frequency_3,
+              "selected_column": column,
+              "selected_column_2": column_2,
+              "selected_column_3": column_3
+          })
