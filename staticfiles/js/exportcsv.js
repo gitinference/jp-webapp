2abbb3ffb4a423e9a9b27fba2ba504972146ff4f
LUIS J SERRATO DIAZ <90709414+luisjomyrproy@users.noreply.github.com>
2025-06-27 12:00:47 -0400

    366 fix nomina sector industrial (#367)
    * se agregan los botones de descargar csv en gastos de gobierno estatal
    
    * cambio de version
    
    * cambios menores en el js y en el html
    
    * se quita el alert
    
    * fix menores nomina por sector indutrial
    
    * cambio de version

--- staticfiles/js/exportcsv.js
+++ staticfiles/js/exportcsv.js
@@ -14,7 +14,7 @@ function downloadCSV(frec, lev, api) {
         frequencyLower = frequency.toLowerCase();
     }
 
-    if (lev == "metric" || lev == "agencies"){
+    if (lev == "metric" || lev == "agencies" || lev == "metric_gasto" || lev == "metric_revenue"){
         level = document.getElementById(lev).value;
         lev  = level;
     }
@@ -30,6 +30,7 @@ function downloadCSV(frec, lev, api) {
     if (levelLower)    params.push("level=" + encodeURIComponent(levelLower));
 
     url = baseUrl + params.join("&");
+
     const a = document.createElement('a');
     a.href = url;
     document.body.appendChild(a);
